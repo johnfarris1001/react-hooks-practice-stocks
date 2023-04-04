243f6a9586b85e004ddd5fe15523817d68a2e083
John <johnfarris217@gmail.com>
2023-04-03 19:37:48 -0500

    deliverables complete

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ stocks, sellStock, buyStock }) {
+  const stocksToDisplay = stocks.map(stock => {
+    return <Stock key={stock.id} stock={stock} buyStock={buyStock} sellStock={sellStock} type='portfolioContainer' />
+  })
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {stocksToDisplay}
     </div>
   );
 }
